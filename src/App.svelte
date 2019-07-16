848d0e317f70242af4094e5f727e408c51534270
priyanjitdey94 <priyanjit.dey@gmail.com>
2019-07-16 21:50:15 +0530

    rendered fusioncharts

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import FusionCharts from './FusionCharts.svelte';
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<FusionCharts />
