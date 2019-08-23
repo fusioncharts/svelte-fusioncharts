a33c4b9dffe2492294f8eafaf4a16259bec3e095
priyanjitdey94 <priyanjit.dey@gmail.com>
2019-08-24 00:11:42 +0530

    created files for samples

---
+++ examples/src/components/samples/SimpleChart.svelte
@@ -0,0 +1,14 @@
+<script>
+  import FusionCharts from 'fusioncharts';
+  import Charts from 'fusioncharts/fusioncharts.charts';
+  import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+  import SvelteFC, { fcRoot } from '../../../../index.mjs';
+
+  import dataSourceStore from '../utils/chart-viewer-data.js';
+
+  fcRoot(FusionCharts, Charts, FusionTheme);
+
+  let chartConfig = dataSourceStore['simple-chart'];
+</script>
+
+<SvelteFC {...chartConfig} />
