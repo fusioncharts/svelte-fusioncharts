8e0dbc2f69e05862e05b64021aa97fe6dc7618d1
priyanjitdey94 <priyanjit.dey@gmail.com>
2019-08-27 20:22:01 +0530

    added samples for demonstrating maps and annotations

---
+++ examples/src/components/samples/UseAnnotations.svelte
@@ -0,0 +1,93 @@
+<script>
+  import FusionCharts from 'fusioncharts';
+  import Charts from 'fusioncharts/fusioncharts.charts';
+  
+  import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+  import SvelteFC, { fcRoot } from '../../../../index.mjs';
+
+  import dataSourceStore from '../utils/chart-viewer-data.js';
+  import { sampleNames } from '../utils/constants.js';
+
+  FusionCharts.options.creditLabel = false;
+
+  fcRoot(FusionCharts, Charts, FusionTheme);
+
+  let chartConfig = {
+    type: 'spline',
+    width: '100%',
+    height: '100%',
+    renderAt: 'chart-container',
+    dataSource: {
+      "chart": {
+        "caption": "Bakersfield Central - Total footfalls",
+        "subCaption": "Last week",
+        "xAxisName": "Day",
+        "yAxisName": "No. of Visitors (In 1000s)",
+        "showValues": "0",
+        "theme": "fusion"
+      },
+      "data": [
+        {
+          "label": "Mon",
+          "value": "15123"
+        },
+        {
+          "label": "Tue",
+          "value": "14233"
+        },
+        {
+          "label": "Wed",
+          "value": "25507"
+        },
+        {
+          "label": "Thu",
+          "value": "9110"
+        },
+        {
+          "label": "Fri",
+          "value": "15529"
+        },
+        {
+          "label": "Sat",
+          "value": "20803"
+        },
+        {
+          "label": "Sun",
+          "value": "19202"
+        }
+      ],
+      "annotations": {
+        "groups": [
+          {
+            "id": "anchor-highlight",
+            "items": [
+              {
+                "id": "high-star",
+                "type": "circle",
+                "x": "$dataset.0.set.2.x",
+                "y": "$dataset.0.set.2.y",
+                "radius": "12",
+                "color": "#6baa01",
+                "border": "2",
+                "borderColor": "#f8bd19"
+              },
+              {
+                "id": "label",
+                "type": "text",
+                "text": "Highest footfall 25.5K",
+                "fillcolor": "#6baa01",
+                "rotate": "90",
+                "x": "$dataset.0.set.2.x+75",
+                "y": "$dataset.0.set.2.y-2"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  };
+</script>
+
+<div id='chart-container' style='height: inherit;' >
+  <SvelteFC {...chartConfig} />
+</div>
---
+++ examples/src/components/samples/WorldMap.svelte
@@ -0,0 +1,91 @@
+<script>
+  import FusionCharts from 'fusioncharts';
+  import Maps from 'fusioncharts/fusioncharts.maps';
+  import World from 'fusioncharts/maps/fusioncharts.world';
+  import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+  import SvelteFC, { fcRoot } from '../../../../index.mjs';
+
+  import dataSourceStore from '../utils/chart-viewer-data.js';
+
+  FusionCharts.options.creditLabel = false;
+
+  fcRoot(FusionCharts, Maps, World, FusionTheme);
+
+  let dataSource = {
+      "chart": {
+        "caption": "Average Annual Population Growth",
+        "subcaption": " 1955-2015",
+        "numbersuffix": "%",
+        "includevalueinlabels": "1",
+        "labelsepchar": ": ",
+        "entityFillHoverColor": "#FFF9C4",
+        "theme": "fusion"
+      },
+      "colorrange": {
+        "minvalue": "0",
+        "code": "#FFE0B2",
+        "gradient": "1",
+        "color": [
+            {
+              "minvalue": "0.5",
+              "maxvalue": "1.0",
+              "color": "#FFD74D"
+            },
+            {
+              "minvalue": "1.0",
+              "maxvalue": "2.0",
+              "color": "#FB8C00"
+            },
+            {
+              "minvalue": "2.0",
+              "maxvalue": "3.0",
+              "color": "#E65100"
+            }
+        ]
+      },
+      "data": [
+        {
+          "id": "NA",
+          "value": ".82",
+          "showLabel": "1"
+
+        },
+        {
+          "id": "SA",
+          "value": "2.04",
+          "showLabel": "1"
+        },
+        {
+          "id": "AS",
+          "value": "1.78",
+          "showLabel": "1"
+        },
+        {
+          "id": "EU",
+          "value": ".40",
+          "showLabel": "1"
+        },
+        {
+          "id": "AF",
+          "value": "2.58",
+          "showLabel": "1"
+        },
+        {
+          "id": "AU",
+          "value": "1.30",
+          "showLabel": "1"
+        }
+      ]
+    },
+    chartConfig = {
+      type: 'world',
+      renderAt: 'chart-container',
+      width: '100%',
+      height: '100%',
+      dataSource
+    };
+</script>
+
+<div id='chart-container' style='height: inherit;' >
+  <SvelteFC {...chartConfig} />
+</div>
